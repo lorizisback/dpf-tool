f7175c9b66f7d3a887f3a694b852a8af37a984e5
ClaudioLoriga <loriga.claudio.ca@gmail.com>
2024-05-26 16:58:37 +0200

    spike: DPF-3 remove useless try catch

--- app/src/main/java/org/dpfTool/presentation/ObdCodeViewModel.kt
+++ app/src/main/java/org/dpfTool/presentation/ObdCodeViewModel.kt
@@ -1,6 +1,5 @@
 package org.dpfTool.presentation
 
-import android.provider.Settings.Global.getString
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -8,10 +7,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import org.dpfTool.R
 import org.dpfTool.domain.GetObdCodeUseCase
 import org.dpfTool.presentation.model.ObdCodeUiState
-import java.io.IOException
 
 class ObdCodeViewModel(private val getObdCodeUseCase: GetObdCodeUseCase) : ViewModel() {
     var uiState by mutableStateOf(ObdCodeUiState())
@@ -22,15 +19,11 @@ class ObdCodeViewModel(private val getObdCodeUseCase: GetObdCodeUseCase) : ViewM
     fun fetchObdCodes() {
         fetchObdCodesJob?.cancel()
         fetchObdCodesJob = viewModelScope.launch {
-            uiState = try {
-                val obdResponse = getObdCodeUseCase.invoke()
-                uiState.copy(
-                    codeName = obdResponse.command.name,
-                    codeValue = obdResponse.formattedValue
-                )
-            } catch (ioe : IOException){
-                uiState.copy(codeName = "error retrieving dpf values")
-            }
+            val obdResponse = getObdCodeUseCase.invoke()
+            uiState = uiState.copy(
+                codeName = obdResponse.command.name,
+                codeValue = obdResponse.formattedValue
+            )
         }
     }
 }
