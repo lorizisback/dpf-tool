40616c4f48abf23313ff262055f7b2c551a1c0ea
ClaudioLoriga <loriga.claudio.ca@gmail.com>
2024-05-26 16:48:36 +0200

    spike: DPF-3 remove unused import

--- app/src/main/java/org/dpfTool/MainActivity.kt
+++ app/src/main/java/org/dpfTool/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import org.dpfTool.presentation.ObdCodeViewModel
 import org.dpfTool.ui.theme.DpfToolTheme
